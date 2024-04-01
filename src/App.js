2a5db3a48a40d94d9ac12566b47cf614a9a8e0f0
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-02 00:52:06 +0500

    Updapted App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
+
+
+// API key = c35a5ed8
+
+const API_URL = 'http://www.omdbapi.com?apikey=c35a5ed8'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+    useEffect(()=>{
+        searchMovies('Spiderman')
+
+    }, []);
+
     return(
+        //react fragment
+        <>
         <h1>Hello, React</h1>
+        </>
     );
 };
 
